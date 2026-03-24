ff0bf3b35e7255939fa580d3d74373a3f73133d2
yusufozdemivr <yusufozdemir.ist@gmail.com>
2026-03-24 10:42:37 +0300

    project skeleton with Spring Boot, PostgreSQL and Docker setup

---
+++ backend/src/main/java/com/yusuf/bist_portfolio_management/BistPortfolioManagementApplication.java
@@ -0,0 +1,13 @@
+package com.yusuf.bist_portfolio_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BistPortfolioManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BistPortfolioManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yusuf/bist_portfolio_management/BistPortfolioManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusuf.bist_portfolio_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BistPortfolioManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
