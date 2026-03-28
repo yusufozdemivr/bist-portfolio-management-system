4c8b9b2f41f79c3343d4585499c9a396f5a6a005
yusufozdemivr <yusufozdemir.ist@gmail.com>
2026-03-28 17:59:12 +0300

    #2 - set up CI/CD pipeline with GitHub Actions, H2 test db, and Checkstyle

--- backend/src/test/java/com/yusuf/bist_portfolio_management/BistPortfolioManagementApplicationTests.java
+++ backend/src/test/java/com/yusuf/bist_portfolio_management/BistPortfolioManagementApplicationTests.java
@@ -2,8 +2,10 @@ package com.yusuf.bist_portfolio_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BistPortfolioManagementApplicationTests {
 
 	@Test
